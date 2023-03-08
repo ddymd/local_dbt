7693604032828f0f74138ad3beb2dbe3f0d20f35
junjun.dong <dongjunjun@gaozhe.com.cn>
2023-03-08 14:14:11 +0800

    testing and documenting the project

--- taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,5 +1,12 @@
 {{ config(materialized="view") }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by "VendorID", lpep_pickup_datetime) as rn
+  from {{ source('staging','green_taxi_trips') }}
+  where 'VendorID' is not null
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(["'vendorid'", "'lpep_pickup_datetime'"]) }} as tripid,
@@ -31,7 +38,8 @@ select
     {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source('staging', 'green_taxi_trips') }}
+from tripdata
+where rn = 1
 
 {% if var('is_test_run', default=true) %}
     limit 100
--- taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -1,5 +1,12 @@
 {{ config(materialized="view") }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by "VendorID", tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_taxi_trips') }}
+  where 'VendorID' is not null 
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(["'vendorid'", "'tpep_pickup_datetime'"]) }} as tripid,
@@ -32,7 +39,8 @@ select
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source('staging', 'yellow_taxi_trips') }}
+from tripdata
+where rn = 1
 
 {% if var('is_test_run', default=true) %}
     limit 100
